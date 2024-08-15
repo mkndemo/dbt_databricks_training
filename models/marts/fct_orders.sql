47d4ddd5cb054fed9bd2078d9369646cf7a9abe1
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-08-15 08:22:59 +0000

    commit

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,37 +1,23 @@
 with customers as (
-
     select * from {{ ref('stg_jaffle_shop__raw_customers') }}
-
 ),
-
 orders as (
-
     select * from {{ ref('stg_jaffle_shop__raw_orders') }}
-
 ),
-
 customer_orders as (
 
     select
         customer_id,
-        min(ordered_at) as first_order_date,
-        max(ordered_at) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        min(ordered_at) as first_order_date
     from orders
     group by 1
 ),
-
 final as (
     select
         customers.customer_id,
         customers.full_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        customer_orders.first_order_date
     from customers
-
     left join customer_orders using (customer_id)
-
 )
-
 select * from final
